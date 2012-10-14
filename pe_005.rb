d903e7d66d468b5b0693fa3c546dc742a8d4ce21
Geeta Vinnakota <geetavin464@gmail.com>
2012-10-13 20:41:42 -0400

    refactor

--- pe_005.rb
+++ pe_005.rb
@@ -4,34 +4,18 @@
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 def is_divisible(num)
-    is_div_flag = true
-    while(is_div_flag == true)
-        i = 20
-        while(i > 1)
-            if(num%i != 0 )
-                is_div_flag = false
-            end
-            break if(is_div_flag == false)
-            i=i-1
-        end
+    for i in 1..20
+       return false if(num%i != 0) 
     end
-    return is_div_flag
+    return true
 end
 
 def smallest_divisible_num
     num = 20
-    found = false
-    while(found == false) 
-        if(is_divisible(num))
-            found = true 
-            smallest_div_num = num
-        end
-        puts "\nCurrent num is"
-        print num
+    loop do 
+        return num if(is_divisible(num))
         num+=20    
     end
-
-    smallest_div_num
 end
 
 smallest_divisible_num
