c2491e2747aac741ab39ed18bdb1f093b141d8cb
Geeta Vinnakota <geetavin464@gmail.com>
2012-10-13 21:00:53 -0400

    refactor

--- pe_010.rb
+++ pe_010.rb
@@ -4,24 +4,20 @@
 
 
 def is_prime(n)
-flag_prime = false
-    if(n == 2)
-        return true
-    else
+    if(n != 2)
         n_sqrt = Math.sqrt(n).round
           for i in 2..n_sqrt
-             break if(n%i == 0)
-             flag_prime = true if( (i==n_sqrt) and n%i != 0 ) 
+              return false if(n%i==0)
           end
     end
-    flag_prime
+    return true
 end
 
 
 def sum_of_primes(limit)   
    sum = 0
        for i in 2...limit
-         sum = sum + i if(is_prime(i))
+         sum+= i if(is_prime(i))
        end 
    sum
 end
@@ -29,4 +25,3 @@ end
 
 sum_of_primes(2000000)
 
-# The block inside inject cannot take conditions !
